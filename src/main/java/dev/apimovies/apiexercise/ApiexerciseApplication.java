e3fb72fd1e31f1967028092e8a23121f0e3b3ced
Juan Munguia <carlosmunguia23145@gmail.com>
2023-12-18 11:36:44 +0100

    created proyect sprint

---
+++ src/main/java/dev/apimovies/apiexercise/ApiexerciseApplication.java
@@ -0,0 +1,13 @@
+package dev.apimovies.apiexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiexerciseApplication.class, args);
+	}
+
+}
